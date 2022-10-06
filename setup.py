20047edd3776884f8130a1068599843c220a0e83
Gaurav <gvkundalwal@gmail.com>
2022-10-07 00:44:22 +0530

    cchange python version

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setuptools.setup(
     extras_require={
         "fast": ["cryptg>=0.2"],
     },
-    python_requires="~=3.10",
+    python_requires="~=3.9",
 
     classifiers=[
         "Development Status :: 4 - Beta",
